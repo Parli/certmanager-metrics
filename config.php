c83f27f1554c35ced32e9da9502b85a071056286
Eric Stern <github@ericstern.com>
2018-12-18 12:57:17 -0800

    Update SDK to get timeouts, set up logging (#2)

--- config.php
+++ config.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 use function DI\autowire;
 use Firehed\API;
 use Firehed\CertStatus\Endpoints;
+use Firehed\SimpleLogger\Stdout;
 use Kubernetes\Api as KubeAPI;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 $builder = new DI\ContainerBuilder();
 $env = getenv('ENVIRONMENT');
@@ -18,10 +20,19 @@ $builder->useAutowiring(true);
 
 $defs = [
     API\Errors\HandlerInterface::class => function ($c) {
-        return new class implements API\Errors\HandlerInterface
+        $logger = $c->get(LoggerInterface::class);
+
+        return new class ($logger) implements API\Errors\HandlerInterface
         {
+            private $logger;
+            public function __construct(LoggerInterface $logger)
+            {
+                $this->logger = $logger;
+            }
+
             function handle($req, $err): ResponseInterface
             {
+                $this->logger->error((string)$err);
                 return new Zend\Diactoros\Response\TextResponse((string)$err, 500);
             }
         };
@@ -33,6 +44,10 @@ $defs = [
         } else {
             return new Kubernetes\LocalProxy('http://localhost:8001');
         }
+    },
+
+    LoggerInterface::class => function () {
+        return new Stdout();
     },
 
     Endpoints\Healthz::class => autowire(),
